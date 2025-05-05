91b9e56ffcbc2690048af86ca518cd869d8fcb4d
Mostafa Motahari <mostafamotahari2004@gmail.com>
2025-05-06 01:11:28 +0330

    chore: update package metadata and setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="persian-ocr",
     version="0.1.0",
     description="PDF/Img to text tool for PDF/Img files those contains Persian contents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Mostafa Motahari",
     author_email="mostafamotahari2004@gmail.com",
     license="Apache",
-    packages=find_packages(where="src"),
-    package_dir={"": "."},
+    url="https://github.com/MostafaMotahari/persian-ocr",
+    packages=find_packages(),
     install_requires=["seleniumbase>=4.38.0"],
     python_requires=">=3.7",
     classifiers=[
